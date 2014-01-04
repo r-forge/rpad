a59f1eaf747bd603dd31ea9efc2ef82a2ea2f09e
jedick (Jeffrey Dick) <j3ffdick@gmail.com>
2014-01-04 10:15:39 +0000

    improve timeout in R_process.pl
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rpad@4 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Rpad/inst/basehtml/server/R_process.pl
+++ pkg/Rpad/inst/basehtml/server/R_process.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl -w
+#!/usr/bin/perl
 #!c:/apps/perl/bin/perl.exe 
 
 # The following line is a test script to see if it works.
@@ -10,14 +10,15 @@ use warnings;
 use Statistics::R;
 use Linux::Inotify2;
 use File::Path qw(remove_tree);
-use Time::Out qw(timeout);
 use CGI qw/:standard send_http_header/;
+use Time::HiRes qw(time sleep);
 
-# chdir to temporary directory
+CGI::initialize_globals();
+
+# get name of temporary directory
 #my $Rpad_ID = '/home/jedick/tmp/rtmp';  ## testing
 my $Rpad_ID = param('ID');
 chomp($Rpad_ID);
-chdir $Rpad_ID;
 
 #my $p_command = 'login';  ## testing
 #my $p_command = 'R_commands';  ## testing
@@ -33,7 +34,7 @@ if ($p_command eq 'login') {
   die "Fork failed: $!" if !defined $pid;
 
   if ($pid == 0) {
-
+    
     # do this in the child
     open STDOUT, ">/dev/null";
     open STDERR, ">/dev/null";
@@ -41,6 +42,7 @@ if ($p_command eq 'login') {
     # start R and load Rpad
     my $R = "";
     sub startR{
+      chdir $Rpad_ID;
       $R = Statistics::R->new( shared => 1 );
       $R->start();
       $R->run(q`require(Rpad)`); 
@@ -48,27 +50,28 @@ if ($p_command eq 'login') {
       # this way the R process behaves more like the interactive session - it isn't halted when an error occurs
       # but errors still hang up the perl so we can't use it...
       #$R->run(q`options(error = expression(NULL))`); 
+      chdir ".." or die "Failed to go to parent directory: $!";
     }
     &startR();
 
     # subroutine to clean up temporary directory
     sub cleanup{
-      chdir ".." or die "Failed to go to parent directory: $!";
       remove_tree($Rpad_ID);
     }
 
     # set up inotify watch on temporary directory
     my $inotify = new Linux::Inotify2
       or die "Unable to create new inotify object: $!";
-    $inotify->watch("$Rpad_ID", IN_CLOSE_WRITE, sub {
+    $inotify->watch($Rpad_ID, IN_CLOSE_WRITE, sub {
       my $event = shift;
       my $name = $event->name;
+      my $fullname = $event->fullname;
       # run the input.R when it appears
       if ( $name eq "input.R" ) {
         # read R command from the input file
         # $R->run_from_file would be cleaner, but it's a source(), so doesn't echo like the interactive session
         #my $output_value = eval { $R->run_from_file($name); };
-        open(my $fh, '<', $name) or die "Could not open file '$name' $!";
+        open(my $fh, '<', $fullname) or die "Could not open file '$fullname' $!";
         # Slurp into a scalar
         my $R_commands; 
           { local $/ = undef; $R_commands = <$fh>; }
@@ -84,7 +87,7 @@ if ($p_command eq 'login') {
           &startR();
         }
         # save output to file
-        my $filename = "output";
+        my $filename = $Rpad_ID . "/output";
         open($fh, '>', $filename) or die "Could not open file '$filename' $!";
         print $fh "$output_value\n";
         close $fh;
@@ -95,15 +98,20 @@ if ($p_command eq 'login') {
       }
     }) or die "watch creation failed: $!";
 
-    # put a limit of 10 minutes on our process
-    timeout 600 => sub {
-      1 while $inotify->poll;
-    } ;
-    if ($@){
-      &cleanup();
-      # operation timed-out
-      die "watch process reached timeout limit";
+    # time out after 5 minutes
+    # Sys::SigAction or Time::Out work when script is run from commandline, but don't timeout under CGI
+    # instead, turn off blocking on inotify and use Time::HiRes
+    # http://www.perlmonks.org/?node_id=859287
+    $inotify->blocking(0);
+    my $timelimit = 20;
+    my $end = time() + $timelimit;
+    while( time() < $end ) {
+      $inotify->poll;
+      sleep 0.1;
     }
+    &cleanup();
+    die "watch process reached timeout limit";
+
   }
 
   # wait a second for R to start and to begin watch of
@@ -131,8 +139,14 @@ elsif ($p_command eq 'R_commands') {
   $R_commands =~ s/\xA0/ /g;
   
   # save commands to the input file that is being watched by the child process
-  my $filename = "input.R";
-  open(my $fh, '>', $filename) or die "Could not open file '$filename' $!";
+  my $filename = $Rpad_ID . "/input.R";
+  sub not_writable{
+    @output_value="Input file not writable; timeout may have occurred.\nTry starting a new session by reloading the page.\n";
+    print header;
+    print @output_value;
+    exit 1;
+  }
+  open(my $fh, '>', $filename) or &not_writable();
   # split the string of commands on newlines
   my @lines = split /\n/, $R_commands;
   foreach my $line (@lines) {
@@ -145,32 +159,40 @@ elsif ($p_command eq 'R_commands') {
   }
   close $fh;
 
-  # set up inotify watch to grab output file
+  # set up inotify watch to grab output file or create message if tmpdir (Rpad_ID) was deleted while running R command
   my $inotify = new Linux::Inotify2
     or die "Unable to create new inotify object: $!";
-  $inotify->watch("$Rpad_ID", IN_CLOSE_WRITE, sub {
+  # set up a watcher on an output file
+  $inotify->watch($Rpad_ID, IN_DELETE_SELF | IN_CLOSE_WRITE, sub {
     my $event = shift;
     my $name = $event->name;
-    if ( $name eq "output" ) {
+    my $fullname = $event->fullname;
+    if ( $event->IN_DELETE_SELF ) {
+      # TODO: test this, do we ever get here?
+      @output_value="Temporary directory was deleted while running R command; timeout may have occurred.\nTry starting a new session by reloading the page.\n";
+    }
+    elsif ( $name eq "output" ) {
       # read contents of output file into output_value
-      my $filename = "output";
-      open(my $fh, '<', $filename) or die "Could not open file '$filename' $!";
+      open(my $fh, '<', $fullname) or die "Could not open file '$fullname' $!";
       @output_value=<$fh>;
       close $fh;
     }
   }) or die "watch creation failed: $!";
 
-  # interrupt the watch if it runs for more than 10 seconds.
-  timeout 10 => sub {
-    $inotify->poll;
-  } ;
-  if ($@){
-    # operation timed-out
-    @output_value = "10 seconds passed with no output from R ... back to you!\n";
-  }
+  $inotify->poll;
+
+#  this slows down response time considerably, so for now running of R commands is blocking
+#  # return a default message if the R process doesn't finish within 10 seconds
+#  @output_value = "10 seconds passed with no output from R ... back to you!\n";
+#  $inotify->blocking(0);
+#  my $timelimit = 10;
+#  my $end = time() + $timelimit;
+#  while( time() < $end ) {
+#    $inotify->poll;
+#    sleep 0.01;
+#  }
 
 }
 
-CGI::initialize_globals();
 print header;
 print @output_value;
